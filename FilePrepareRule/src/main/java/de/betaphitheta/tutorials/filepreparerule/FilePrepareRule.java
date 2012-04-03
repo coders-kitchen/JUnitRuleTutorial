7715ae02ee6a841489d97e7b9609ac93a43f2910
Peter Daum <pedaum@web.de>
2012-04-04 00:32:58 +0200

    Added Apache v2.0 licensing and comments

--- FilePrepareRule/src/main/java/de/betaphitheta/tutorials/filepreparerule/FilePrepareRule.java
+++ FilePrepareRule/src/main/java/de/betaphitheta/tutorials/filepreparerule/FilePrepareRule.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *Copyright 2012 Peter Daum aka mrpaeddah
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
  */
 package de.betaphitheta.tutorials.filepreparerule;
 
@@ -13,7 +24,7 @@ import org.junit.runners.model.Statement;
 /**
  * This implementation of MethodRule returns a FilePrepareStatement which 
  * creates and deletes the requested file structure for a test method.
- * @author mrpaeddah
+ * @author peter
  */
 public class FilePrepareRule implements MethodRule {
     public Statement apply(final Statement base, FrameworkMethod method, Object target) {
